f27d34fef7b86b499152bbc277f0094114e7970d
lexus22206 <lexusp715@gmail.com>
2026-01-08 23:35:34 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// hidden-header
+const header = document.querySelector(".header");
+let lastScroll = 0;
+const offset = 80;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+
+  header.classList.toggle("is-scrolled", currentScroll > 10);
+
+  if (currentScroll <= offset) {
+    header.classList.remove("is-hidden");
+    lastScroll = currentScroll;
+    return;
+  }
+
+  if (currentScroll > lastScroll) {
+    header.classList.add("is-hidden");
+  } else {
+    header.classList.remove("is-hidden");
+  }
+
+  lastScroll = currentScroll;
+});
+
 // burger
 const burger = document.querySelector(".header__burger");
 const menu = document.querySelector(".menu");
