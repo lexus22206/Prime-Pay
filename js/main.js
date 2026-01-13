62652262a59b77fec764100d262cba858ce5f639
lexus22206 <lexusp715@gmail.com>
2026-01-14 00:43:22 +0200

    section hero + feature

--- js/main.js
+++ js/main.js
@@ -34,3 +34,12 @@ burger.addEventListener("click", () => {
   body.classList.toggle("menu-open");
   menu.classList.toggle("open");
 });
+
+// multiline
+document.querySelectorAll(".feature__title").forEach((title) => {
+  const lineHeight = parseFloat(getComputedStyle(title).lineHeight);
+  const lines = Math.round(title.offsetHeight / lineHeight);
+  if (lines > 1) {
+    title.classList.add("multiline");
+  }
+});
